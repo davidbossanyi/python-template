736d3a5b0b03ccc1e82363dcf2b87c0cc79d2ea5
davidbossanyi <davebossanyi@gmail.com>
2022-05-19 22:34:16 +0100

    Initial commit

---
+++ tests/src/test_main.py
@@ -0,0 +1,7 @@
+import pytest
+from src.main import hello_world
+
+
+@pytest.mark.parametrize("name", ["spam", "eggs"])
+def test_hello_world(name: str) -> None:
+    assert hello_world(name=name) == f"Hello {name}!"
