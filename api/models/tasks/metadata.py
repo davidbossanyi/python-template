54305b39d39514cabfd6b48a5f671bc60f7270b9
davidbossanyi <davebossanyi@gmail.com>
2022-10-16 23:17:39 +0100

    Create alias for task id in CeleryTaskMetaData model

--- api/models/tasks/metadata.py
+++ api/models/tasks/metadata.py
@@ -1,6 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class CeleryTaskMetaData(BaseModel):
-    id: str
+    id: str = Field(..., alias="task_id")
     status: str
+
+    class Config:
+        allow_population_by_field_name = True
